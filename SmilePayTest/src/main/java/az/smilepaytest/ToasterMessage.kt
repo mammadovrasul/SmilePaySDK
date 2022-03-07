b301864edd6526f927a94b6b5b5ffba938001722
r.mammadov <mammadov.rasul36@gmail.com>
2022-03-08 01:12:37 +0400

    3-cu

--- SmilePayTest/src/main/java/az/smilepaytest/ToasterMessage.kt
+++ SmilePayTest/src/main/java/az/smilepaytest/ToasterMessage.kt
@@ -4,7 +4,7 @@ import android.content.Context
 import android.widget.Toast
 
 
-class ToasterMessage {
+object ToasterMessage {
     fun showMessage(c: Context?, message: String?) {
         Toast.makeText(c, message, Toast.LENGTH_SHORT).show()
     }
